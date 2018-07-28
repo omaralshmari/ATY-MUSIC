4a5ee4e8c15656592e4615e9fec9b2a91cbd8c3c
omaralshmari <41575972+omaralshmari@users.noreply.github.com>
2018-07-28 22:35:13 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,8 +7,7 @@ const getYoutubeID = require('get-youtube-id');
 const fetchVideoInfo = require('youtube-info');
 
 const yt_api_key = "AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4";
-const prefix = 'بريفكس';
-const discord_token = "توكن";
+const prefix = '1';
 client.login(discord_token);
 client.on('ready', function() {
     console.log(`i am ready ${client.user.username}`);
