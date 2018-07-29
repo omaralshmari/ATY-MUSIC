c5382a0f8204eb2b62b977a26bc3d285d0fa6be6
omaralshmari <41575972+omaralshmari@users.noreply.github.com>
2018-07-29 15:22:31 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,16 +1,14 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const ytdl = require('ytdl-core');
-const request = require('request');
-const fs = require('fs');
-const getYoutubeID = require('get-youtube-id');
-const fetchVideoInfo = require('youtube-info');
-
-const yt_api_key = "AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4";
-const prefix = '1';
-client.on('ready', function() {
-    console.log(`i am ready ${client.user.username}`);
+ const prefix = "برفكسك";
+client.on('ready', () => {
+    console.log('I am ready!');
 });
+
+client.on('message', message => {
+    if (message.content === 'zg') {
+        message.reply('pong');
+      }
 /*
 ////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\
 ////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\
