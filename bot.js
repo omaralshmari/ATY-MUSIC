c7077b92e9febed15a1f67cd1793e1f70d8ba80c
omaralshmari <41575972+omaralshmari@users.noreply.github.com>
2018-07-29 11:25:24 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,10 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const ytdl = require('ytdl-core');
+const request = require('request');
+const fs = require('fs');
+const getYoutubeID = require('get-youtube-id');
+const fetchVideoInfo = require('youtube-info');
  const prefix = "1";
 client.on('ready', () => {
     console.log('I am ready!');
