4fe92c30e6117fa2fc9a06c7632594c39415ee54
omaralshmari <41575972+omaralshmari@users.noreply.github.com>
2018-07-29 11:15:28 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
- const prefix = "برفكسك";
+ const prefix = "1";
 client.on('ready', () => {
     console.log('I am ready!');
 });
